b38d94d1d2064384ff407185ed30007510d48148
kaleblub <kaleblub@gmail.com>
2022-04-05 23:21:11 +0300

    I added the ability to add an item to the saved list. Also added the ability to print the whole saved list. Still need to add in indexing for the saved list so that I can delete accordingly.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # what-to-do
+---
+## Description
 A Rust program that is a type of CLI to-do list for people that have a short attention span and often forget the different tasks that they have left to accomplish. Can give a random idea for something that you can spend your time on, must add the things as you think of them and delete them as you complete them.
+
+---
+## Current Features
+- Can use 'add', 'delete', and 'print-all' subcommands with clap.
+- 'add' asks for the task to append to the list of tasks in the save file.
+- 'print-all' reads the save file and outputs the whole list.
+
+---
+## To-do
+- Must get each line in the file to be listed with a reference number for it.
+- Must be able to run the 'delete' subcommand and then choose which index to delete.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use clap::{Arg, Command};
+use std::fs::OpenOptions;
+use std::io::{Write, Read, stdin};
+
+const FILE_NAME: &str = ".what-to-do.dat";
 
 fn main() {
+    // Create 'my_file' new if it does not exist, or open if it does exist.
+    // std::fs::File::create(FILE_NAME).expect("creation failed");
+
+    // Using clap to add subcommands
     let matches = Command::new("What To Do? (To-do list)")
         .version("1.0")
         .author("Kaleb H. <kaleblub.pythonanywhere.com>")
@@ -21,11 +29,31 @@ fn main() {
 
 
     // Check for existence of subcommands
-    if let Some(ref matches) = matches.subcommand_matches("add") {
-        println!("Add something");
-    } else if let Some(ref matches) = matches.subcommand_matches("delete") {
-        println!("Delete something")
-    } else if let Some(ref matches) = matches.subcommand_matches("print-all") {
-        println!("Print all.")
+    if let Some(ref _matches) = matches.subcommand_matches("add") {
+        let mut file = OpenOptions::new().append(true).create(true).open(&FILE_NAME).unwrap(); //.expect("Unable to add item");
+        let mut input_string = String::new();
+
+        // Get the input for what task to add to list
+        println!("What task would you like to store to be reminded about?");
+        stdin().read_line(&mut input_string)
+            .ok()
+            .expect("Failed to read line");
+        
+        // Write the user-given line to the file or return an error
+        if let Err(e) = write!(file, "{}", input_string) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
+        println!("Added {}", input_string);
+
+    } else if let Some(ref _matches) = matches.subcommand_matches("delete") {
+        // my_file.write_all("Delete something".as_bytes()).expect("delete failed");
+        
+        println!("Deleted something");
+    } else if let Some(ref _matches) = matches.subcommand_matches("print-all") {
+        // my_file.write_all("Print all".as_bytes()).expect("print all failed");
+        let mut file = std::fs::File::open(FILE_NAME).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        println!("{}", contents);
     }
 }
